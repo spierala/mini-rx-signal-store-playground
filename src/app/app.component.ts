2bde95f4d3df49d6eabe80f1f9dfd6af95c6e591
Florian Spier <mail@florian-spier.be>
2023-04-24 20:59:22 +0200

    Play with Signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, computed, effect, signal} from '@angular/core';
+import {BehaviorSubject, map, tap} from "rxjs";
+import {toSignal} from "@angular/core/rxjs-interop";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <p>Signal: {{doubleCounter()}}</p>
+    <p>Observable: {{doubleCounter$ | async}}</p>
+    <button (click)="inc()">Inc</button>
+  `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-signals';
+  // BehaviorSubject
+  counterBehaviorSubject = new BehaviorSubject(1);
+  doubleCounter$ = this.counterBehaviorSubject.pipe(
+    map(v => v * 2),
+    tap(() => console.log('RxJS piped'))
+  )
+
+  // Signal
+  counterSignal = toSignal(this.counterBehaviorSubject, {requireSync: true});
+  doubleCounter = computed(() => {
+    const result = this.counterSignal() * 2
+    console.log('Signal computed')
+    return result;
+  });
+
+  inc() {
+    console.log('BEFORE');
+
+    // this.counterSignal.update(v => v + 1);
+    this.counterBehaviorSubject.next(this.counterBehaviorSubject.value + 1);
+
+    console.log('AFTER');
+  }
+
+  // Logging:
+  // BEFORE
+  // RxJS piped
+  // AFTER
+  // Signal computed
 }
